2c598cd273aa592ffb20527b19ab23069e677748
Nikitenko Max <nikitenkomax70@gmail.com>
2021-03-10 22:18:19 +0300

    feat: add function to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    if (array) {
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        return array[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    if (array) {
+        array.sort(function (a, b) {
+            return b - a;
+        });
+        return array[0];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    if (array) {
+        return (
+            array.reduce((total, item) => {
+                return (total += item);
+            }, 0) / array.length
+        );
+    }
+};
